d3c2ddc3169229d63e7ee19f5870327dbb68f6e5
mark <markpackham1@gmail.com>
2022-01-20 16:36:20 +0000

    load sprites and add test map

--- src/main.js
+++ src/main.js
@@ -9,3 +9,58 @@ kaboom({
   height: 240,
   scale: 2,
 });
+
+loadRoot("sprites/");
+loadAseprite("mario", "Mario.png", "Mario.json");
+loadAseprite("enemies", "enemies.png", "enemies.json");
+loadSprite("ground", "ground.png");
+loadSprite("questionBox", "questionBox.png");
+loadSprite("emptyBox", "emptyBox.png");
+loadSprite("brick", "brick.png");
+loadSprite("coin", "coin.png");
+loadSprite("bigMushy", "bigMushy.png");
+loadSprite("pipeTop", "pipeTop.png");
+loadSprite("pipeBottom", "pipeBottom.png");
+loadSprite("shrubbery", "shrubbery.png");
+loadSprite("hill", "hill.png");
+loadSprite("cloud", "cloud.png");
+loadSprite("castle", "castle.png");
+
+const LEVELS = [
+  [
+    "                                                                                                ",
+    "                                                                                                ",
+    "                                                                                                ",
+    "                                                                                                ",
+    "                                                                                                ",
+    "                                                                                                ",
+    "                                                                                                ",
+    "      -?-b-                                                                                     ",
+    "                                                    ?        ?                                  ",
+    "                                                                                                ",
+    "                                      _                 ?                                       ",
+    "                                 _    |                                                         ",
+    "                           _     |    |                _                                        ",
+    "       E                   |     |    |   E   E        |                            H           ",
+    "================     ===========================================================================",
+    "================     ===========================================================================",
+  ],
+  [
+    "                                                                                             ",
+    "                                                                                             ",
+    "                                                                                             ",
+    "                                       ?                                                     ",
+    "                                                                                             ",
+    "                                   -?-                                                       ",
+    "                                                                                             ",
+    "      -?-b-                  -?-                                                             ",
+    "                                                                                             ",
+    "                                                                                             ",
+    "                                                                                             ",
+    "                                                                                             ",
+    "       _                                            _                                        ",
+    "       |                                            |          E    E            H           ",
+    "================     ========================================================================",
+    "================     ========================================================================",
+  ],
+];
